3f299f6084498e18a5ce113e2b64fd9fd917ecec
Morteza <Morteza@DESKTOP-GTKGRP7>
2020-05-16 18:31:03 +0430

    Add another sample

--- src/Samples/Mousavi.Extensions.Configuration.SqlServer.Samples/Program.cs
+++ src/Samples/Mousavi.Extensions.Configuration.SqlServer.Samples/Program.cs
@@ -7,15 +7,30 @@ namespace Mousavi.Extensions.Configuration.SqlServer.Samples
     {
         static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
+            var configuration1 = new ConfigurationBuilder()
                 .AddSqlServer("Server=localhost;Database=Example;User Id=sa;Password=your(#SecurePassword!123)")
                 .Build();
 
-            foreach (var config in configuration.AsEnumerable())
+            foreach (var config in configuration1.AsEnumerable())
             {
-                Console.Write($"Key: {config.Key}, Value: {config.Value}");
+                Console.WriteLine($"Key: {config.Key}, Value: {config.Value}");
             }
 
+
+            var configuration2 = new ConfigurationBuilder()
+                .AddSqlServer(c =>
+                {
+                    c.ConnectionString =
+                        "Server=localhost;Database=Example;User Id=sa;Password=your(#SecurePassword!123)";
+                    c.Schema = "config";
+                    c.Table = "Settings";
+                })
+                .Build();
+
+            foreach (var config in configuration2.AsEnumerable())
+            {
+                Console.WriteLine($"Key: {config.Key}, Value: {config.Value}");
+            }
         }
     }
 }
